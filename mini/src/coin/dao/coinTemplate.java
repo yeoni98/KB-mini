0343f96bac70caff6220e6a73e5c43ed58449eaf
신혜영 <nanda5050@naver.com>
2023-04-06 10:12:48 +0900

    feat: Add basic template

--- mini/src/coin/dao/coinTemplate.java
+++ mini/src/coin/dao/coinTemplate.java
@@ -1,5 +1,9 @@
 package coin.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import coin.vo.Account;
@@ -7,6 +11,10 @@ import coin.vo.Customer;
 import coin.vo.WalletNo;
 
 public interface coinTemplate {
+	Connection getConnect() throws SQLException;
+	void closeAll(PreparedStatement ps, Connection conn)throws SQLException;
+	void closeAll(ResultSet rs, PreparedStatement ps, Connection conn)throws SQLException;
+	
 	// 비즈니스 로직
 	void insertCust(Customer cust);
 	void updateCust(Customer cust);
@@ -15,7 +23,7 @@ public interface coinTemplate {
 
 	void openAccount(int custId);
 	int getAccountStatus(int accountNo);
-	int getAccountBalance();
+	int getAccountBalance(int accountNo);
 	
 	void createWallet(int custId);
 	void deleteWallet(String walNo);
@@ -26,8 +34,8 @@ public interface coinTemplate {
 	void buyCoin(int custId, String coinCode);
 	void sellCoin(int custId, String coinCode);
 	
-	void deductBalance(String accountNo, int amount);
-	void addBalance(String accountNo, int amount);
+	void deductBalance(String custId, int amount);
+	void addBalance(String custId, int amount);
 	
 	void insertWalletLog(WalletNo wl);
 }
